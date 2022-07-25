193097946c1d6b9430e926c4afa38717aff0aaa6
Divya Sathiyamoorthy <sdivya2408@gmail.com>
2022-07-25 14:25:23 -0500

    added frontend for health stats

--- Medicross/frontend/src/pages/HealthStats/HealthStats.js
+++ Medicross/frontend/src/pages/HealthStats/HealthStats.js
@@ -1,9 +1,57 @@
 import React, { useRef, useState, useEffect } from "react";
 import "./HealthStats.css";
 import axios from "axios";
-import TextField from "@mui/material/TextField";
+import TextField from '@mui/material/TextField';
 
 const HealthStats = () => {
-    
+  const userRef = useRef();
+  const errRef = useRef();
+  const [disabledField, setDisabledField] = useState(true);
+  const [condition, setCondition] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false); //for testing purposes only and needs to be replaced with react-router
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.get("http://localhost:3002/api/findrisks", {
+        condition: condition,
+      });
+      
+      console.log(response.data);
+      setCondition(response.data);
+      setSuccess(true);
+    } catch (err) {
+      if (!err.response) {
+        setErrMsg("No Server Response");
+      } else {
+        setErrMsg("Health Stats Failed");
+      }
+      errRef.current.focus();
+    }
+  };
+
+  return (
+    <>
+        {success ? (
+            <section>
+            <h1>Conditions</h1>
+            <br />
+            <TextField
+                disabled={disabledField}
+                value={condition} />
+            </section>
+        ) : (
+            <section>
+                <form onSubmit={handleSubmit}>
+                    <button>
+                        View Common Conditions
+                    </button>
+                </form>
+            </section>
+        )}
+    </>
+  );
 };
+
 export default HealthStats;
